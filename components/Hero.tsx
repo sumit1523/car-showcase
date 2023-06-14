dee4a3eb2ef925d2b9c161a896fbc46fe7cb1b83
sumit kumar <sumit1523@gmail.com>
2023-06-15 04:50:31 +0530

    Hero section and Navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,9 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title">Find, Book, or Rent a Car - quickly</h1>
+        <h1 className="hero__title">
+          Find, Book, or Rent a Car - quickly and easily!
+        </h1>
         <p className="hero__subtitle">
           Streamline your car rental experience with our effortless booking
           process
